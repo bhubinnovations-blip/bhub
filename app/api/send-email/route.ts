e36426b694b5f819f7d12bd1453ee3a73861ad67
bhubinnovations-blip <bhubinnovations@gmail.com>
2025-09-04 19:32:11 +0300

    Add files via upload

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,32 @@
+"use server"
+
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+export async function POST(req: Request) {
+
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    try {
+
+        const { to, subject, html } = await req.json();
+        const email = {
+            from: 'BHub <cawine@cawineapp.com>',
+            to: [to],
+            subject: subject,
+            html: html
+        }
+
+        const response = await resend.emails.send(email);
+
+        console.log(response);
+
+        return NextResponse.json({ success: true, response });
+
+    } catch (error: any) {
+        console.error("Error sending email:", error);
+        return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+    }
+
+}
