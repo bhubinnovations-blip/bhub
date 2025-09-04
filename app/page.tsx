098e5359590c15d73b3fa7234c6518e9ad414da0
bhubinnovations-blip <bhubinnovations@gmail.com>
2025-09-04 19:38:09 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+
 "use client";
 
 import { useEffect, useRef, useState } from "react";
 import feather from "feather-icons";
 import Image from "next/image";
-import { title } from "process";
+import { useRouter } from "next/navigation";
 
 interface Tile {
   icon?: string;
@@ -29,6 +30,7 @@ const tiles: Tile[] = [
 ];
 
 export default function HomePage() {
+  const router = useRouter();
   const [scrolled, setScrolled] = useState(false);
   const [notifications] = useState(3);
   const [cartItems, setCartItems] = useState(2);
@@ -159,19 +161,19 @@ export default function HomePage() {
                 if (tile.isButton) {
                 } else if (tile.isClient) {
                   localStorage.setItem("bundle", "user");
-                  window.location.assign("/signin.html");
+                  router.push("/signin");
                 } else if (tile.isSalon) {
                   localStorage.setItem("bundle", "salon");
-                  window.location.assign("/signin.html");
+                  router.push("/signin");
                 } else if (tile.isBeauty) {
                   localStorage.setItem("bundle", "beauty");
-                  window.location.assign("/signin.html");
+                  router.push("/signin");
                 } else if (tile.isGYM) {
                   localStorage.setItem("bundle", "gym");
-                  window.location.assign("/signin.html");
+                  router.push("/signin");
                 } else if (tile.isTrainer) {
                   localStorage.setItem("bundle", "trainer");
-                  window.location.assign("/signin.html");
+                  router.push("/signin");
                 }
               }}
             >
@@ -231,3 +233,92 @@ export default function HomePage() {
     </div>
   );
 }
+
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+//       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
+//         <Image
+//           className="dark:invert"
+//           src="/b_hub_logo.png"
+//           alt="bhub logo"
+//           width={180}
+//           height={38}
+//           priority
+//         />
+//         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+//           <li className="mb-2 tracking-[-.01em]">
+//             Get started on Bhub As:.
+//           </li>
+//           <li className="tracking-[-.01em]">
+//             Login if you got an account.
+//           </li>
+//         </ol>
+
+//         <div className="flex gap-4 items-center flex-col sm:flex-row">
+//           <a
+//             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+//             href="/landing.html"
+//             rel="noopener noreferrer"
+//           >
+            
+//             Continue As
+//           </a>
+//           <a
+//             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+//             href="/login.html"
+//             rel="noopener noreferrer"
+//           >
+//             Login
+//           </a>
+//         </div>
+//       </main>
+//       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+//           href="/aboutus.html"
+//           rel="noopener noreferrer"
+//         >
+//           <Image
+//             aria-hidden
+//             src="/file.svg"
+//             alt="File icon"
+//             width={16}
+//             height={16}
+//           />
+//           About Bhub
+//         </a>
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+//           href="/contactus.html"
+//           rel="noopener noreferrer"
+//         >
+//           <Image
+//             aria-hidden
+//             src="/window.svg"
+//             alt="Window icon"
+//             width={16}
+//             height={16}
+//           />
+//           Contact Us
+//         </a>
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+//           href="/aboutus.html"
+//           rel="noopener noreferrer"
+//         >
+//           <Image
+//             aria-hidden
+//             src="/globe.svg"
+//             alt="Globe icon"
+//             width={16}
+//             height={16}
+//           />
+//           Vist our website
+//         </a>
+//       </footer>
+//     </div>
+//   );
+// }
